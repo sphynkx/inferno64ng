044e8b10bd3fa51bcf1b56a1aa9786dd84a0df76
sphynkx <sfynkx@gmail.com>
2026-05-17 03:27:29 +0300

    Icurses: Implement TextView - for rich text presentations in viewer/editor. Draft stage.

---
+++ module/icurses/textview.m
@@ -0,0 +1,40 @@
+include "icurses/view.m";
+
+IcTextView: module
+{
+	PATH: con "/dis/lib/icurses/textview.dis";
+
+	Span: adt
+	{
+		line: int;
+		start: int;
+		end: int;
+		code: string;
+	};
+
+	Model: adt
+	{
+		lines: array of string;
+		spans: array of Span;
+
+		topline: int;
+		leftcol: int;
+
+		basecode: string;
+		sanitizecode: string;
+		selectcode: string;
+		searchcode: string;
+	};
+
+	init: fn();
+
+	new: fn(): ref Model;
+	clear: fn(m: ref Model);
+
+	setlines: fn(m: ref Model, lines: array of string);
+	setscroll: fn(m: ref Model, topline, leftcol: int);
+	setcodes: fn(m: ref Model, basecode, sanitizecode, selectcode, searchcode: string);
+	setspans: fn(m: ref Model, spans: array of Span);
+
+	linecount: fn(m: ref Model): int;
+};
